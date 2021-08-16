91e69c03034ce687e7d0812277ff9faf9638d985
Aubri Henley <aubri.henley@gmail.com>
2021-08-16 17:11:40 -0400

    added question arrays

--- index.js
+++ index.js
@@ -1,14 +1,98 @@
 const inquierer = require('inquirer');
 const fs = require('fs');
-const Employee = require ('./lib/Employee.js');
-const Engineer = require ('./lib/Engineer.js');
-const Manager = require ('./lib/Manager.js');
-const Employees = [];
+const Employee = require('./lib/Employee.js');
+const Engineer = require('./lib/Engineer.js');
+const Manager = require('./lib/Manager.js');
+
+//Question arrays
+
+const managerQuestions = [{
+        type: "input",
+        message: "Enter team's manager name:",
+        name: "name"
+    },
+    {
+        type: "input",
+        message: `"Enter ${this.name}'s employee ID number:"`,
+        name: "id"
+    },
+    {
+        type: "input",
+        message: `"Enter ${this.name}'s email:"`,
+        name: "email"
+    },
+    {
+        type: "input",
+        message: `"Enter ${this.name}'s offic number:"`,
+        name: "officeNumber"
+    },
+    {
+        type: "list",
+        message: "If you would like to add a new team member select type of team member, or select done.",
+        choices: ["Engineer", "Intern", "Done"],
+        name: "addEmployee"
+    },
+];
+
+const engineerQuestions = [{
+    type: "input",
+    message: "Enter engineer's name:",
+    name: "name"
+},
+{
+    type: "input",
+    message: `"Enter ${this.name}'s employee ID number:"`,
+    name: "id"
+},
+{
+    type: "input",
+    message: `"Enter ${this.name}'s email:"`,
+    name: "email"
+},
+{
+    type: "input",
+    message: `"Enter ${this.name}'s GitHub username:"`,
+    name: "github"
+},
+{
+    type: "list",
+    message: "If you would like to add a new team member select type of team member, or select done.",
+    choices: ["Engineer", "Intern", "Done"],
+    name: "addEmployee"
+},
+];
+
+const internQuestions = [{
+    type: "input",
+    message: "Enter intern's name:",
+    name: "name"
+},
+{
+    type: "input",
+    message: `"Enter ${this.name}'s employee ID number:"`,
+    name: "id"
+},
+{
+    type: "input",
+    message: `"Enter ${this.name}'s email:"`,
+    name: "email"
+},
+{
+    type: "input",
+    message: `"Enter ${this.name}'s school:"`,
+    name: "github"
+},
+{
+    type: "list",
+    message: "If you would like to add a new team member select type of team member, or select done.",
+    choices: ["Engineer", "Intern", "Done"],
+    name: "addEmployee"
+},
+];
 
 
-
-const generateHTML =(answers) =>
-`<!DOCTYPE html>
+const generateHTML = (answers) =>
+    `<!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
@@ -30,4 +114,3 @@ const generateHTML =(answers) =>
 </div>
 </body>
 </html>`;
-
